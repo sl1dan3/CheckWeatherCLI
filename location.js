afa80e116110bbba55d42b0454eb7bf74d715c90
Arseniy Dementyev <tkkggqq@gmail.com>
2026-08-20 23:51:29 +0300

    feat: add getUserLocation function to fetch coordinates via IP geolocation

---
+++ location.js
@@ -0,0 +1,26 @@
+const getUserLocation = async () => {
+    try {
+        const response = await fetch("https://ipwho.is/");
+
+        if (!response.ok) {
+            throw new Error("Failed to get your geolocation")
+        }
+
+        const userLocationInfo = await response.json();
+
+        if (!userLocationInfo.success) {
+            throw new Error("Failed to get user location")
+        }
+
+        const userCoordinates = {
+            latitude: userLocationInfo.latitude,
+            longitude: userLocationInfo.longitude,
+        }
+
+        return userCoordinates;
+    } catch (error) {
+        console.log(`Error: ${error.message}`)
+    }
+}
+
+export {getUserLocation};
